472a59aa3698180c93fbbe2e591ba46ad1871b88
Coninckx David <dco@open-net.ch>
2016-06-23 11:50:30 +0200

    Mapping on contract position

--- eagle_migration/models/contract.py
+++ eagle_migration/models/contract.py
@@ -5,13 +5,16 @@
 
 from openerp import models, fields, api
 
-
+#Import logger
+import logging
+#Get the logger
+_logger = logging.getLogger(__name__)
 
 class EagleContract(models.Model):
     _inherit = 'eagle.contract'
 
     @api.multi
-    def migrate_positions(self):
+    def migrate_positions(self, template):
         map_recurrency = {
             'day': 'daily',
             'week': 'weekly',
@@ -22,7 +25,9 @@ class EagleContract(models.Model):
             sale_sub_obj = self.env['sale.subscription']
             sale_sub = sale_sub_obj.create({
                 'eagle_contract': contract.id,
-                'name': 'test'
+                'name': 'test',
+                'date_start': contract.date_start,
+                'template_id': template.id,
             })
             for position in self.positions:
                 sale_sub_line_obj = self.env['sale.subscription.line']
@@ -35,9 +40,23 @@ class EagleContract(models.Model):
                     'product_id': position.name.id,
                     'recurring_interval': position.recurrence_id.value,
                     'recurring_rule_type': map_recurrency.get(position.recurrence_id.unit)  or 'none',
-                    'requested_date': position.next_invoice_date,
+                    'recurring_next_date': position.next_invoice_date,
                     'is_billable': position.is_billable,
                     'is_active': position.is_active,
+                    'sequence': position.sequence,
+                    'cancellation_deadline': position.cancellation_deadline,
+                    'discount': position.discount,
                 })
 
 
+class EagleContract(models.TransientModel):
+    _name = 'eagle.contract.migration.wizard'
+
+    eagle_subscription_template = fields.Many2one('sale.subscription', domain="[('type', '=', 'template')]", string="Subscription template")
+    
+    @api.multi
+    def migrate_positions(self):
+        
+        active_ids = self._context.get('active_ids', []) or []
+        contracts = self.env['eagle.contract'].browse(active_ids)
+        contracts.migrate_positions(self.eagle_subscription_template)
