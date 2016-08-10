43a32be2cb4dc516e6d3c362a565e4a6d70227d1
Y-Phil <cyp@open-net.ch>
2016-08-10 18:48:17 +0200

    UPD: bouton from analytic account to project now leads to a list if there's more than one project.

--- eagle_project/models/analytic.py
+++ eagle_project/models/analytic.py
@@ -62,3 +62,23 @@ class AccountAnalyticLine(models.Model):
             new_line.write({'eagle_contract': eagle_contract.id})
 
         return new_line_id
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    @api.multi
+    def projects_action(self):
+        project_ids = sum([account.project_ids.ids for account in self], [])
+        result = {
+            "type": "ir.actions.act_window",
+            "res_model": "project.project",
+            "views": [[False, "tree"], [False, "form"]],
+            "domain": [["id", "in", project_ids]],
+            "context": {"create": False},
+            "name": "Projects",
+        }
+        if len(project_ids) == 1:
+            result['views'] = [(False, "form")]
+            result['res_id'] = project_ids[0]
+
+        return result
