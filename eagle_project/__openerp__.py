123d72ba15892e36ded21d61c29d08b49eb21949
Y-Phil <cyp@open-net.ch>
2016-12-12 00:45:19 +0100

    Corrections traductions, fenetres et menus inutiles

--- eagle_project/__openerp__.py
+++ eagle_project/__openerp__.py
@@ -8,7 +8,7 @@
 #  Copyright (c) 2011-TODAY Open-Net Ltd. <http://www.open-net.ch>
 {
     'name' : 'Eagle View: project module',
-    'version' : '9.2.06',
+    'version' : '9.2.09',
     'author' : 'Open Net Sàrl',
     'category': 'Eagle view',
     'summary': 'Synthetic view on your business',
