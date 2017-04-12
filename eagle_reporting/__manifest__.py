aa583921273580a4e7d91810fb41edff62be8914
Open Net Sarl <jae@open-net.ch>
2017-04-12 16:31:51 +0200

    Updated eagle contract report - Added a style as seen in opennet cust - Added fields page_break and do_page_break to control the page breaks - Updated report_contracts to 10.0 standards

--- eagle_reporting/__manifest__.py
+++ eagle_reporting/__manifest__.py
@@ -7,25 +7,6 @@
 #  MIG[10.0] by lfr@open-net.ch (2017)
 #
 #  Copyright (c) 2015-TODAY Open-Net Ltd. <http://www.open-net.ch>
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-TODAY OpenERP S.A. <http://www.openerp.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
 {
     'name' : 'Eagle View: reporting module',
     'version' : '10.0.0.1',
@@ -34,17 +15,15 @@
     'category': 'Eagle view',
     'summary': 'Synthetic view on your business',
     'website': 'http://www.open-net.ch',
-    'depends' : ['eagle_project'],
+    'depends' : [
+        'eagle_project',
+        'ons_productivity_layout'
+    ],
     'data': [
+        'views/eagle_reporting.xml',
         'views/report_contracts.xml',
-    ],
-    'js': [
-    ],
-    'qweb' : [
-    ],
-    'css':[
-    ],
-    'demo': [
+        'views/view_contracts.xml',
+        'views/view_sale_subscription.xml'
     ],
     'test': [
     ],
---
+++ eagle_reporting/models/contracts.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+# © 2017 Open Net Sarl
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from itertools import groupby
+from odoo import _, api, fields, models
+
+import logging
+_logger = logging.getLogger(__name__)
+
+
+class EagleContract(models.Model):
+    _inherit = 'eagle.contract'
+
+    do_page_break = fields.Boolean(
+        string="Do a page break between subscriptions types",
+        help="Do a page break between recurent and non recurent subscriptions in the report")
+
+    @api.multi
+    def subs_lines_layouted(self, filtred_lines):
+        """
+        Returns this order lines classified by sale_layout_category and separated in
+        pages according to the category pagebreaks. Used to render the report.
+        """
+        self.ensure_one()
+        report_pages = [[]]
+        for category, lines in groupby(filtred_lines, lambda l: l.sale_layout_cat_id):
+            # If last added category induced a pagebreak, this one will be on a new page
+            if report_pages[-1] and report_pages[-1][-1]['pagebreak']:
+                report_pages.append([])
+            # Append category to current report page
+            report_pages[-1].append({
+                'name': category and category.name or 'Uncategorized',
+                'subtotal': category and category.subtotal,
+                'pagebreak': category and category.pagebreak,
+                'lines': list(lines)
+            })
+        
+        new_layout = [[]]
+
+        for page in report_pages:
+            for category in page:
+                has_page_break = False
+                for line in category['lines']:
+                    if line.page_break:
+                        has_page_break = True
+                if has_page_break:
+                    _logger.info(category["name"])
+                    new_layout[-1].append(category)
+                    new_layout.append([])
+                else:
+                    _logger.info(category["name"])
+                    new_layout[-1].append(category)
+
+        return new_layout
